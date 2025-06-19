da1d2357c54f5887d01ed9e06abf00e458ddb86c
Carlo Castillo <carlo89512@yahoo.com>
2025-06-18 22:27:13 -0700

    working on the sidebar

--- spotify-clone/components/sidebar.tsx
+++ spotify-clone/components/sidebar.tsx
@@ -4,6 +4,9 @@ import React, { useMemo } from 'react';
 import { usePathname } from 'next/navigation';
 import { HiHome } from 'react-icons/hi';
 import { BiSearch } from 'react-icons/bi';
+import SidebarItem from './SidebarItem';
+import Box from './box';
+
 
 interface SidebarProps {
     children: React.ReactNode;
@@ -19,7 +22,7 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
             label: 'Home',
             active: pathname !== '/search',
             href: '/',
-        },        
+        },
         {
             icon: BiSearch,
             label: 'Search',
@@ -31,10 +34,20 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
         <div className="flex h-full">
             <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2 ">
                 <Box>
-                    Sidebar Navigation
+                    <div className="flex flex-col gap-y-4 px-5 py-4">
+                        {routes.map((item) => (
+                            <SidebarItem key={item.label} {...item} />
+                        ))}
+                    </div>
+                </Box>
+                <Box className="overflow-y-auto h-full">
+                    Song Library
                 </Box>
 
             </div>
+            <main className="h-full flex-1 overflow-y-auto py-2">
+                {children}
+            </main>
         </div>
     );
 }
