5cd598e36907c12fc6be406767127050266995a4
Carlo Castillo <carlo89512@yahoo.com>
2025-06-19 21:47:48 -0700

    setup supabase

---
+++ spotify-clone/providers/ModalProvider.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import Modal from "@/components/Modal";
+import { useEffect, useState } from "react";
+
+const ModalProvider = () => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if (!isMounted) {
+        return null;
+    }
+
+    return(
+        <>
+            <Modal title="Test Modal" description="Test Desciption" isOpen onChange={() => {}}>
+                Test Children
+            </Modal>
+        </>
+    );
+}
+
+export default ModalProvider;
