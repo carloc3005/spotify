b2de4150f342af27bd6e606b3723a91f805b74d2
carloc3005 <carlo89512@yahoo.com>
2025-06-23 18:33:22 -0700

    Add MediaItem component to SearchContent
    Integrated the MediaItem component into the SearchContent component to display individual songs. Also improved the empty state message when no songs are found.

--- spotify-clone/app/search/components/SearchContent.tsx
+++ spotify-clone/app/search/components/SearchContent.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Song } from "@/types";
+import MediaItem from "../../../components/MediaItem"
 
 interface SearchContentProps {
     songs: Song[];
@@ -12,9 +13,25 @@ const SearchContent: React.FC<SearchContentProps> = ({
     if(songs.length === 0)
     return(
         <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">
-
+            No songs found
         </div>
     );
+
+
+    return (
+        <div className="flex flex-col gap-y-2 w-full px-6">
+            {songs.map((song) => (
+                <div key={song.id} className="flex items-center gap-x-4 w-full ">
+                    <div className="flex-1">
+                        <MediaItem
+
+                    </div>
+                     
+                </div>
+            ))}
+        </div>
+    );
+
 }
 
 
